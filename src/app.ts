2b2ab2dafbffbf6c9f41c37b471440ff8453d9cc
jroberto-inv <jroberto@inv.com.vc>
2022-04-20 15:24:56 -0300

    create class Sender and methods

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log('teste')
+import { create, Whatsapp, Message, SocketState } from 'venom-bot'
+
+class Sender {
+  private client: Whatsapp
+
+  constructor() {
+
+  }
+
+  private initialize() {
+    const qrcode = () => {
+
+    }
+    const status = () => {
+
+    }
+    const start = (client: Whatsapp) => {
+
+    }
+
+    create('ws-sender-dev', qrcode, status)
+      .then((client) => start(client))
+      .catch((error) => console.error(error))
+  }
+}
+
