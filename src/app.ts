4c08a463cd729205d154f1373f7daefbf14f0c2b
jroberto-inv <jroberto@inv.com.vc>
2022-04-20 16:07:29 -0300

    rename file app to sender

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,3 @@
-import { create, Whatsapp, Message, SocketState } from 'venom-bot'
+import Sender from './sender'
 
-class Sender {
-  private client: Whatsapp
-
-  constructor() {
-
-  }
-
-  private initialize() {
-    const qrcode = (base64Qrimg: string) => {
-      console.log()
-    }
-    const status = (statusSession: string) => {
-
-    }
-    const start = (client: Whatsapp) => {
-      this.client = client
-    }
-
-    create('ws-sender-dev', qrcode, status)
-      .then((client) => start(client))
-      .catch((error) => console.error(error))
-  }
-}
-
-export default Sender
+const sender = new Sender()
